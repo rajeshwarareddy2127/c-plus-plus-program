1acdaf0f5c8f3f6954ce7f9f7f00b664a566a567
rajeshwarareddy2127 <125203939+rajeshwarareddy2127@users.noreply.github.com>
2024-02-12 14:40:03 +0530

    Add files via upload

---
+++ round the float value.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+	float a=2.4,b=7.7;
+	int z=floor(a);
+	 int x=floor(b);
+	cout <<"round values is :" << z << endl;
+	cout << "round value is :" << x << endl;
+}
+/*
+output:
+round values is :2
+round value is :7
+*/
