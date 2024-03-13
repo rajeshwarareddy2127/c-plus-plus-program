922e29f2027117a84dde1baf04a24725755490e5
rajeshwarareddy2127 <125203939+rajeshwarareddy2127@users.noreply.github.com>
2024-03-13 20:33:41 +0530

    Add files via upload

---
+++ implicity conversition.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int x,z;
+	char y;
+	cin >> x;
+	cin  >> y;
+	x=x+y;
+	z=x+2.8;
+	cout << "x=" << x << endl;
+	cout << "y=" << y << endl;
+	cout << "z=" << z;
+	
+}
