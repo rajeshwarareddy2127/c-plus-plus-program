613838c4fa4f96082b7f85a449dd19302e9c2ef9
rajeshwarareddy2127 <125203939+rajeshwarareddy2127@users.noreply.github.com>
2024-03-13 20:32:43 +0530

    Add files via upload

---
+++ corresponding month.cpp
@@ -0,0 +1,52 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int monthnum;
+	cin >> monthnum;
+	if(monthnum<1 || monthnum>12)
+	{
+		cout <<"invalid month";
+		return 1;
+	}
+	switch(monthnum)
+	{
+		case 1:
+			cout <<"january";
+			break;
+		case 2:
+			cout <<"february";
+			break;
+		case 3:
+			cout <<"march";
+			break;
+		case 4:
+			cout <<"april";
+			break;
+		case 5:
+			cout <<"may";
+			break;
+		case 6:
+			cout <<"june";
+			break;
+		case 7:
+			cout <<"july";
+			break;
+		case 8:
+			cout <<"august";
+			break;
+		case 9:
+			cout <<"september";
+			break;
+		case 10:
+			cout <<"octomber";
+			break;
+		case 11:
+			cout <<"november";
+			break;
+		case 12:
+			cout <<"december";
+			break;
+	}
+	
+}
