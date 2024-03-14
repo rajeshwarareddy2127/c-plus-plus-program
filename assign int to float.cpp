718b455b3ef1354910ebe1b03f43359e8c795a77
rajeshwarareddy2127 <125203939+rajeshwarareddy2127@users.noreply.github.com>
2024-03-14 09:46:21 +0530

    Add files via upload

---
+++ assign int to float.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+int main() {
+   
+    int integerVariable = 42;
+
+    
+    float floatVariable;
+
+   
+    floatVariable = integerVariable;
+
+    
+    std::cout << "Integer Variable: " << integerVariable << std::endl;
+    std::cout << "Float Variable: " << floatVariable << std::endl;
+
+    return 0;
+}
+
