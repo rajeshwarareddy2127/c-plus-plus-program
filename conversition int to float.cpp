372439e53895507b4c785ee69ecbc7a31ceb8daf
rajeshwarareddy2127 <125203939+rajeshwarareddy2127@users.noreply.github.com>
2024-03-14 09:47:00 +0530

    Add files via upload

---
+++ conversition int to float.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+   
+    int integerVariable = 42;
+
+   
+    float floatVariable = integerVariable;
+
+   
+    std::cout << "Integer Variable: " << integerVariable << std::endl;
+    std::cout << "Float Variable: " << floatVariable << std::endl;
+
+    return 0;
+}
+
